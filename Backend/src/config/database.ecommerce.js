386b3ccc4d601c4b582de0d85af80d8533973e82
Parvesh <arveshchaudhary@gmail.com>
2026-01-14 19:53:51 +0545

    Connecting mongoDB

---
+++ Backend/src/config/database.ecommerce.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+
+        console.log(
+            `✅ MongoDB Connected: ${connect.connection.name}`
+        );
+    } catch (error) {
+        console.error("❌ MongoDB connection failed:", error.message);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
